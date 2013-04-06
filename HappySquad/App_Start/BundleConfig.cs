fa69d5878f0e6eee8a0110bce159157e4d284fea
Pavel <alterwaza@gmail.com>
2013-04-06 23:39:27 +0400

    Починил плагин dataTables.

--- HappySquad/App_Start/BundleConfig.cs
+++ HappySquad/App_Start/BundleConfig.cs
@@ -7,36 +7,43 @@
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(
+                new ScriptBundle("~/bundles/jqueryui").Include(
+                    "~/Scripts/jquery-ui-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(
+                new ScriptBundle("~/bundles/jqueryval").Include(
+                    "~/Scripts/jquery.unobtrusive*",
+                    "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dataTables").Include("~/Scripts/jquery.dataTables*"));
+
+            bundles.Add(
+                new StyleBundle("~/Content/themes/base/dataTables").Include("~/Content/jquery.dataTables_themeroller.css"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(
+                new StyleBundle("~/Content/themes/base/css").Include(
+                    "~/Content/themes/base/jquery.ui.core.css",
+                    "~/Content/themes/base/jquery.ui.resizable.css",
+                    "~/Content/themes/base/jquery.ui.selectable.css",
+                    "~/Content/themes/base/jquery.ui.accordion.css",
+                    "~/Content/themes/base/jquery.ui.autocomplete.css",
+                    "~/Content/themes/base/jquery.ui.button.css",
+                    "~/Content/themes/base/jquery.ui.dialog.css",
+                    "~/Content/themes/base/jquery.ui.slider.css",
+                    "~/Content/themes/base/jquery.ui.tabs.css",
+                    "~/Content/themes/base/jquery.ui.datepicker.css",
+                    "~/Content/themes/base/jquery.ui.progressbar.css",
+                    "~/Content/themes/base/jquery.ui.theme.css",
+                    "~/Content/themes/base/jquery.dataTables.css"));
         }
     }
 }
